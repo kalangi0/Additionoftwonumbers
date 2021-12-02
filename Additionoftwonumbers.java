6bf61bb4b7ff2d4556d3429a065b68b185a6419b
kalangi0 <91305185+kalangi0@users.noreply.github.com>
2021-12-02 11:12:40 +0530

    Add files via upload

---
+++ Additionoftwonumbers.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PEC-IT
+ */
+public class Additionoftwonumbers {
+    public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+    
+}
